2a99132d94468c8756c6baaecca33b3c6c660c0c
Kosyrev143 <artemkosyrev93@gmail.com>
2024-12-01 23:20:27 +0500

    first commit

---
+++ resources/views/user/create.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.main')
+
+@section('title', 'Register page')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <h1 class="h2">Register page</h1>
+
+            <form action="{{route('user.store')}}" method="post">
+                @csrf
+
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name</label>
+                    <input name="name" type="text" class="form-control" id="email" placeholder="Name">
+                </div>
+
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input name="email" type="email" class="form-control" id="email" placeholder="Email">
+                </div>
+
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input name="password" type="password" class="form-control" id="password" placeholder="Password">
+                </div>
+
+                <div class="mb-3">
+                    <label for="password_confirmation" class="form-label">Confirm Password</label>
+                    <input name="password_confirmation" type="password" class="form-control" id="password_confirmation" placeholder="Confirm Password">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Register</button>
+                <a href="{{route('login')}}" class="ms-3">Already registered?</a>
+            </form>
+        </div>
+    </div>
+@endsection
